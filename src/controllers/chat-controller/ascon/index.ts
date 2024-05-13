887343cf481f322ac64d20a8156e74e687125225
felipenasci <dfnasciimento@live.com>
2024-05-13 14:09:53 -0300

    feat: add ticket confirmation

--- src/controllers/chat-controller/ascon/index.ts
+++ src/controllers/chat-controller/ascon/index.ts
@@ -1,11 +1,37 @@
 import { State } from "../interface";
 import { ASCON } from "../../../domain/menu-options/ascon";
-import { SUCCESS_ANSWER } from "../../../domain/menu-options/shared/success";
+import {
+  ETicketConfirmationOptions,
+  SUCCESS_ANSWER,
+  TICKET_CONFIRMATION,
+} from "../../../domain/menu-options/shared/success";
+import { MenuOptions } from "../../../domain/menu-options/interface";
+import { extractValuesFromObject } from "../../../helpers";
+import { asconConfirmationTemplate } from "../../../templates/ticket-confirmation";
+
+const ticketConfirmationOptions = extractValuesFromObject<string>(
+  ETicketConfirmationOptions
+);
 
 export const AsconMenuOptionsState: State = {
   menu: ASCON,
-  next: () => AnswerAsconState,
+  next: (_, ticket) => {
+    const message = [
+      TICKET_CONFIRMATION.message,
+      asconConfirmationTemplate(ticket),
+      " ",
+    ].join("\n");
+
+    const menu: MenuOptions = { ...TICKET_CONFIRMATION, message };
+    return TicketConfirmationState({ menu });
+  },
 };
+
+export const TicketConfirmationState = (params?: State): State => ({
+  menu: params.menu || TICKET_CONFIRMATION,
+  next: (choice) =>
+    ticketConfirmationOptions.includes(choice) && AnswerAsconState,
+});
 
 export const AnswerAsconState: State = {
   type: "service",
