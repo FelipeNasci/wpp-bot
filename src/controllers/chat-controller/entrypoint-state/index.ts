07a6dd07b58d5646cd8212f2b5b0389f4d11eaba
felipenasci <dfnasciimento@live.com>
2024-07-21 14:57:01 -0300

    feat: add gtic options

--- src/controllers/chat-controller/entrypoint-state/index.ts
+++ src/controllers/chat-controller/entrypoint-state/index.ts
@@ -3,6 +3,7 @@ import {
   ENTRYPOINT_MENU_OPTIONS,
   EntrypointOptions,
 } from "../../../domain/menu-options";
+import { GticMenuOptionsState } from "../gtic";
 import { AsconMenuOptionsState } from "../ascon";
 import { AdministrativeConsultancyMenuOptionsState } from "../administrative-consultancy";
 import { PuMaintenanceDepartmentKindState } from "../university-town-hall";
@@ -11,6 +12,8 @@ export const InitialMenuState: State = {
   menu: ENTRYPOINT_MENU_OPTIONS,
   next: (choice) => {
     switch (choice) {
+      case EntrypointOptions.GTIC:
+        return GticMenuOptionsState;
       case EntrypointOptions.ASCON:
         return AsconMenuOptionsState;
       case EntrypointOptions.ADMINISTRATIVE_CONSULTANCY:
